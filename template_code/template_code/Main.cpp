41393106b576da6efac07378c79234b4d19a0927
Michael Ferguson <mferg3838@gmail.com>
2024-04-28 19:13:49 -0700

    small fixes

--- template_code/template_code/Main.cpp
+++ template_code/template_code/Main.cpp
@@ -1,144 +1,184 @@
-#include "Edge.h"
+
 #include <iostream>
+#include <fstream>
 #include <limits>
-#include <string>
-#include <sstream>
+#include "Edge.h"
 
 using namespace std;
 
-// Define MAX_VERTICES as a constant
-const int MAX_VERTICES = 9999;
+class Graph
+{
+    int V;
+    int **adj;
 
-// Declare Graph structure and functions outside main
-struct Graph {
-    int numOfVertices;
-    bool adjacencyMatrix[MAX_VERTICES][MAX_VERTICES];
-
-    //Initialize matrix
-    Graph(int vertices) : numOfVertices(vertices) {
-        for (int i = 0; i < numOfVertices; ++i) {
-            for (int j = 0; j < numOfVertices; ++j) {
-                adjacencyMatrix[i][j] = false;
+public:
+    Graph(int vertices) : V(vertices)
+    {
+        adj = new int *[V];
+        for (int i = 0; i < V; ++i)
+        {
+            adj[i] = new int[V];
+            for (int j = 0; j < V; ++j)
+            {
+                adj[i][j] = 0;
             }
         }
     }
 
-    // Method to add an edge to the graph
-    void addEdge(int startVertex, int endVertex) {
-        adjacencyMatrix[startVertex - 1][endVertex - 1] = true;
-        adjacencyMatrix[endVertex - 1][startVertex - 1] = true;
+    ~Graph()
+    {
+        for (int i = 0; i < V; ++i)
+        {
+            delete[] adj[i];
+        }
+        delete[] adj;
+    }
+
+    void addEdge(int u, int v)
+    {
+        adj[u][v] = 1;
+        adj[v][u] = 1;
+    }
+
+    void printAdjacencyMatrix()
+    {
+        cout << "Adjacency matrix of G:" << endl;
+        for (int i = 0; i < V; ++i)
+        {
+            for (int j = 0; j < V; ++j)
+            {
+                cout << adj[i][j] << " ";
+            }
+            cout << endl;
+        }
+    }
+
+    int *findOddDegreeVertices()
+    {
+        int *oddVertices = new int[V];
+        int count = 0;
+        for (int i = 0; i < V; ++i)
+        {
+            int degree = 0;
+            for (int j = 0; j < V; ++j)
+            {
+                degree += adj[i][j];
+            }
+            if (degree % 2 != 0)
+            {
+                oddVertices[count++] = i + 1;
+            }
+        }
+        oddVertices[count] = -1; // Mark end of array
+        return oddVertices;
+    }
+
+    int *dijkstra(int src)
+    {
+        int *dist = new int[V];
+        bool *visited = new bool[V];
+
+        for (int i = 0; i < V; ++i)
+        {
+            dist[i] = numeric_limits<int>::max();
+            visited[i] = false;
+        }
+
+        dist[src] = 0;
+
+        for (int count = 0; count < V - 1; ++count)
+        {
+            int u = minDistance(dist, visited);
+            visited[u] = true;
+            for (int v = 0; v < V; ++v)
+            {
+                if (!visited[v] && adj[u][v] && dist[u] != numeric_limits<int>::max() && dist[u] + 1 < dist[v])
+                {
+                    dist[v] = dist[u] + 1;
+                }
+            }
+        }
+
+        delete[] visited;
+        return dist;
+    }
+
+private:
+    int minDistance(const int *dist, const bool *visited)
+    {
+        int minDist = numeric_limits<int>::max();
+        int minIndex = -1;
+        for (int v = 0; v < V; ++v)
+        {
+            if (!visited[v] && dist[v] <= minDist)
+            {
+                minDist = dist[v];
+                minIndex = v;
+            }
+        }
+        return minIndex;
     }
 };
 
-// Function to print the adjacency matrix of the graph
-void printAdjacencyMatrix(Graph &graph) {
-    cout << "\t\tThe adjacency matrix of G:\n";
-    for (int i = 0; i < graph.numOfVertices; ++i) {
-        for (int j = 0; j < graph.numOfVertices; ++j) {
-            cout << graph.adjacencyMatrix[i][j] << " ";
-        }
-        cout << endl;
+int main(int argc, char *argv[])
+{
+    if (argc != 2)
+    {
+        cout << "Usage: ./dijkstra <graph_file>" << endl;
+        return 1;
     }
-}
 
-// Function to print the odd degree vertices in the graph
-void printOddDegreeVertices(Graph &graph) {
-    cout << "\t\tThe odd degree vertices in G:\nO = { ";
-    for (int i = 0; i < graph.numOfVertices; ++i) {
-        int degree = 0;
-        for (int j = 0; j < graph.numOfVertices; ++j) {
-            if (graph.adjacencyMatrix[i][j]) {
-                degree++;
+    ifstream infile(argv[1]);
+    if (!infile)
+    {
+        cout << "Error: Unable to open file " << argv[1] << endl;
+        return 1;
+    }
+
+    int n, m;
+    infile >> n >> m;
+
+    Graph g(n);
+
+    for (int i = 0; i < m; ++i)
+    {
+        int u, v;
+        infile >> u >> v;
+        g.addEdge(u - 1, v - 1);
+    }
+
+    g.printAdjacencyMatrix();
+
+    int *oddVertices = g.findOddDegreeVertices();
+    cout << "\nThe odd degree vertices in G:" << endl;
+    cout << "O = { ";
+    for (int i = 0; oddVertices[i] != -1; ++i)
+    {
+        cout << oddVertices[i] << " ";
+    }
+    cout << "}" << endl;
+
+    for (int i = 0; oddVertices[i] != -1; ++i)
+    {
+        int vertex = oddVertices[i];
+        cout << "\nSingle source shortest path lengths from node " << vertex << endl;
+        int *shortestPaths = g.dijkstra(vertex - 1);
+        for (int j = 0; j < n; ++j)
+        {
+            cout << "  " << j + 1 << ": ";
+            if (shortestPaths[j] == numeric_limits<int>::max())
+            {
+                cout << "INF" << endl;
+            }
+            else
+            {
+                cout << shortestPaths[j] << endl;
             }
         }
-        if (degree % 2 != 0) {
-            cout << i + 1 << " ";
-        }
-    }
-    cout << "}\n";
-}
-
-// Function to implement Dijkstra's algorithm
-void dijkstra(Graph &graph, int startVertex) {
-    const int INF = numeric_limits<int>::max();
-    int dist[graph.numOfVertices];
-    bool visited[graph.numOfVertices] = {false};
-
-    for (int i = 0; i < graph.numOfVertices; ++i) {
-        dist[i] = INF;
+        delete[] shortestPaths;
     }
 
-    dist[startVertex - 1] = 0;
-
-    for (int count = 0; count < graph.numOfVertices - 1; ++count) {
-        int u = -1;
-        for (int i = 0; i < graph.numOfVertices; ++i) {
-            if (!visited[i] && (u == -1 || dist[i] < dist[u])) {
-                u = i;
-            }
-        }
-
-        visited[u] = true;
-
-        for (int v = 0; v < graph.numOfVertices; ++v) {
-            if (!visited[v] && graph.adjacencyMatrix[u][v] && dist[u] != INF &&
-                dist[u] + 1 < dist[v]) {
-                dist[v] = dist[u] + 1;
-            }
-        }
-    }
-
-    cout << "Single source shortest path lengths from node " << startVertex << endl;
-    for (int i = 0; i < graph.numOfVertices; ++i) {
-        cout << "  " << i + 1 << ": " << dist[i] << endl;
-    }
-    cout << endl;
-}
-
-int main() {
-    // start of the initial pipeline that loads the test case file stream from std::cin
-    int numOfVertices;
-    int numOfEdges;
-
-    if (!std::cin.eof()) {
-        std::cin >> numOfVertices;
-        std::cin >> numOfEdges;
-    } else {
-        std::cout << "Input not found!" << std::endl;
-        return 0;
-    }
-
-    // Declare Graph object outside the loop
-    Graph myGraph(numOfVertices);
-
-    while (!std::cin.eof()) {
-        int startVertice;
-        int endVertice;
-        std::cin >> startVertice;
-        std::cin >> endVertice;
-        Edge *newEdge = new Edge(startVertice, endVertice);
-        // Here is where you load up the Graph object
-        myGraph.addEdge(startVertice, endVertice);
-    }
-
-    // And here is where you start working on the three tasks
-    // Call the functions to print adjacency matrix, odd degree vertices, and run Dijkstra's algorithm
-    printAdjacencyMatrix(myGraph);
-    printOddDegreeVertices(myGraph);
-    // Assuming you have identified odd degree vertices
-    for (int i = 1; i <= numOfVertices; ++i) {
-        // Run Dijkstra's algorithm for each odd degree vertex
-        int degreeCount = 0;
-
-        for (int j = 0; j < numOfVertices; ++j) {
-            if (myGraph.adjacencyMatrix[i - 1][j]) {
-                degreeCount++;
-            }
-        }
-        if (degreeCount % 2 != 0) {
-            dijkstra(myGraph, i);
-        }
-    }
+    delete[] oddVertices;
 
     return 0;
 }
