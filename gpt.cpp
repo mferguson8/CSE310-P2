41c60ebce12f0311637af293bedf86dc3aff5471
Michael Ferguson <mferg3838@gmail.com>
2024-04-28 19:21:20 -0700

    gpt cpp

--- gpt.cpp
+++ gpt.cpp
@@ -4,52 +4,66 @@
 
 using namespace std;
 
-class Graph {
+class Graph
+{
     int V;
     int **adj;
 
 public:
-    Graph(int vertices) : V(vertices) {
-        adj = new int*[V];
-        for (int i = 0; i < V; ++i) {
+    Graph(int vertices) : V(vertices)
+    {
+        adj = new int *[V];
+        for (int i = 0; i < V; ++i)
+        {
             adj[i] = new int[V];
-            for (int j = 0; j < V; ++j) {
+            for (int j = 0; j < V; ++j)
+            {
                 adj[i][j] = 0;
             }
         }
     }
 
-    ~Graph() {
-        for (int i = 0; i < V; ++i) {
+    ~Graph()
+    {
+        for (int i = 0; i < V; ++i)
+        {
             delete[] adj[i];
         }
         delete[] adj;
     }
 
-    void addEdge(int u, int v) {
+    void addEdge(int u, int v)
+    {
         adj[u][v] = 1;
         adj[v][u] = 1;
     }
 
-    void printAdjacencyMatrix() {
-        cout << "Adjacency matrix of G:" << endl;
-        for (int i = 0; i < V; ++i) {
-            for (int j = 0; j < V; ++j) {
+    void printAdjacencyMatrix()
+    {
+        cout << "The adjacency matrix of G:" << endl;
+        for (int i = 0; i < V; ++i)
+        {
+            for (int j = 0; j < V; ++j)
+            {
                 cout << adj[i][j] << " ";
             }
             cout << endl;
         }
     }
 
-    int* findOddDegreeVertices() {
-        int* oddVertices = new int[V];
+    int *findOddDegreeVertices()
+    {
+        int *oddVertices = new int[V];
         int count = 0;
-        for (int i = 0; i < V; ++i) {
+        for (int i = 0; i < V; ++i)
+        {
             int degree = 0;
-            for (int j = 0; j < V; ++j) {
+            for (int j = 0; j < V; ++j)
+            {
                 degree += adj[i][j];
             }
-            if (degree % 2 != 0) {
+            if (degree % 2 != 0)
+            {
                 oddVertices[count++] = i + 1;
             }
         }
@@ -57,37 +71,59 @@ public:
         return oddVertices;
     }
 
-    int* dijkstra(int src) {
-        int* dist = new int[V];
-        bool* visited = new bool[V];
+    void dijkstra(int src)
+    {
+        int *dist = new int[V];
+        bool *visited = new bool[V];
 
-        for (int i = 0; i < V; ++i) {
+        for (int i = 0; i < V; ++i)
+        {
             dist[i] = numeric_limits<int>::max();
             visited[i] = false;
         }
 
         dist[src] = 0;
 
-        for (int count = 0; count < V - 1; ++count) {
+        for (int count = 0; count < V - 1; ++count)
+        {
             int u = minDistance(dist, visited);
             visited[u] = true;
-            for (int v = 0; v < V; ++v) {
-                if (!visited[v] && adj[u][v] && dist[u] != numeric_limits<int>::max() && dist[u] + 1 < dist[v]) {
+            for (int v = 0; v < V; ++v)
+            {
+                if (!visited[v] && adj[u][v] && dist[u] != numeric_limits<int>::max() && dist[u] + 1 < dist[v])
+                {
                     dist[v] = dist[u] + 1;
                 }
             }
         }
 
+        cout << "\nSingle source shortest path lengths from node " << src + 1 << endl;
+        for (int i = 0; i < V; ++i)
+        {
+            cout << "  " << i + 1 << ": ";
+            if (dist[i] == numeric_limits<int>::max())
+            {
+                cout << "INF" << endl;
+            }
+            else
+            {
+                cout << dist[i] << endl;
+            }
+        }
+
+        delete[] dist;
         delete[] visited;
-        return dist;
     }
 
 private:
-    int minDistance(const int* dist, const bool* visited) {
+    int minDistance(const int *dist, const bool *visited)
+    {
         int minDist = numeric_limits<int>::max();
         int minIndex = -1;
-        for (int v = 0; v < V; ++v) {
-            if (!visited[v] && dist[v] <= minDist) {
+        for (int v = 0; v < V; ++v)
+        {
+            if (!visited[v] && dist[v] <= minDist)
+            {
                 minDist = dist[v];
                 minIndex = v;
             }
@@ -96,14 +132,17 @@ private:
     }
 };
 
-int main(int argc, char* argv[]) {
-    if (argc != 2) {
+int main(int argc, char *argv[])
+{
+    if (argc != 2)
+    {
         cout << "Usage: ./dijkstra <graph_file>" << endl;
         return 1;
     }
 
     ifstream infile(argv[1]);
-    if (!infile) {
+    if (!infile)
+    {
         cout << "Error: Unable to open file " << argv[1] << endl;
         return 1;
     }
@@ -113,7 +152,8 @@ int main(int argc, char* argv[]) {
 
     Graph g(n);
 
-    for (int i = 0; i < m; ++i) {
+    for (int i = 0; i < m; ++i)
+    {
         int u, v;
         infile >> u >> v;
         g.addEdge(u - 1, v - 1);
@@ -121,27 +161,18 @@ int main(int argc, char* argv[]) {
 
     g.printAdjacencyMatrix();
 
-    int* oddVertices = g.findOddDegreeVertices();
+    int *oddVertices = g.findOddDegreeVertices();
     cout << "\nThe odd degree vertices in G:" << endl;
     cout << "O = { ";
-    for (int i = 0; oddVertices[i] != -1; ++i) {
+    for (int i = 0; oddVertices[i] != -1; ++i)
+    {
         cout << oddVertices[i] << " ";
     }
     cout << "}" << endl;
 
-    for (int i = 0; oddVertices[i] != -1; ++i) {
-        int vertex = oddVertices[i];
-        cout << "\nSingle source shortest path lengths from node " << vertex << endl;
-        int* shortestPaths = g.dijkstra(vertex - 1);
-        for (int j = 0; j < n; ++j) {
-            cout << "  " << j + 1 << ": ";
-            if (shortestPaths[j] == numeric_limits<int>::max()) {
-                cout << "INF" << endl;
-            } else {
-                cout << shortestPaths[j] << endl;
-            }
-        }
-        delete[] shortestPaths;
+    for (int i = 0; oddVertices[i] != -1; ++i)
+    {
+        g.dijkstra(oddVertices[i] - 1);
     }
 
     delete[] oddVertices;
